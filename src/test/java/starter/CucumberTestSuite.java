fceef12cda21be779939aa87a716c18e2127a3af
Adhit <prananta.adhitya87@gmail.com>
2022-03-12 00:04:43 +0700

    add update

--- src/test/java/starter/CucumberTestSuite.java
+++ src/test/java/starter/CucumberTestSuite.java
@@ -3,12 +3,14 @@ package starter;
 
 import io.cucumber.junit.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
+import net.serenitybdd.junit.runners.SerenityRunner;
 import org.junit.runner.RunWith;
+
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         plugin = {"pretty"},
-        features = "src/test/resources/features"
-//        tags = "SignIn"
+        features = "src/test/resources/features",
+        tags = "@Update"
 )
 public class CucumberTestSuite {}
--- src/test/resources/features/Register.feature
+++ src/test/resources/features/Register.feature
@@ -2,5 +2,5 @@ Feature: User Register
 
 @SignUp
 Scenario: as a new user, I do to register to the page
-When I input my name is "Q-0Q" and email is "QE-h2@xx.com" and password is "QExx(*&"
+When I input my name is "cup1-4o85" and email is "u/@ucup.com" and password is "io977"
 Then I can do register
