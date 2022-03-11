556257f06287fd531aba93e3e40b0049dc86f588
Adhit <prananta.adhitya87@gmail.com>
2022-03-12 04:25:10 +0700

    add new services

--- src/test/java/starter/CucumberTestSuite.java
+++ src/test/java/starter/CucumberTestSuite.java
@@ -11,6 +11,6 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {"pretty"},
         features = "src/test/resources/features",
-        tags = "@Update"
+        tags = "@NewServices"
 )
 public class CucumberTestSuite {}
