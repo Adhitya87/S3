c26acbe9754f51668d20b99a0b1626dd0466e142
Adhit <prananta.adhitya87@gmail.com>
2022-03-12 21:37:00 +0700

    add feature Edit Service Image and update data services

--- src/test/java/starter/CucumberTestSuite.java
+++ src/test/java/starter/CucumberTestSuite.java
@@ -11,6 +11,6 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {"pretty"},
         features = "src/test/resources/features",
-        tags = "@AllServices"
+        tags = "@EditImageServices"
 )
 public class CucumberTestSuite {}
